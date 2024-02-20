da22ef9ca7bd834c15f9eedf76542e9221dd9b23
Sanjay kandpal <72781686+sanjay-kandpal@users.noreply.github.com>
2024-02-20 11:55:46 +0530

    Runtime: 0 ms (100.00%), Memory: 44.5 MB (99.37%) - Gitcode

---
+++ 268-missing-number/268. Missing Number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int ans[] = new int[nums.length+1];
+        for(int i = 0 ; i< nums.length ;i++){
+           ans[nums[i]] = 1;
+        }
+        for(int i = 0 ; i < ans.length ; i++){
+            if(ans[i] == 0)
+            return i;
+        }
+        return 0;
+    }
+}
