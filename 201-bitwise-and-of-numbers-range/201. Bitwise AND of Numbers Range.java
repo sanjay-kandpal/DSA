221fe2b5b71835e3077ff3f521efa3e8d0291377
Sanjay kandpal <72781686+sanjay-kandpal@users.noreply.github.com>
2024-02-22 20:39:15 +0530

    Runtime: 3 ms (100.00%), Memory: 43.8 MB (68.86%) - Gitcode

--- 201-bitwise-and-of-numbers-range/201. Bitwise AND of Numbers Range.java
+++ 201-bitwise-and-of-numbers-range/201. Bitwise AND of Numbers Range.java
@@ -1,15 +1,18 @@
 class Solution {
     public int rangeBitwiseAnd(int left, int right) {
-        int shift = 0;
-        
-        // Find the common prefix of the binary representations of left and right
-        while (left < right) {
-            left >>= 1;
-            right >>= 1;
-            shift++;
-        }
-        
-        // Shift back to the left to get the final result
-        return left << shift;
+     while(right > left)
+      right = right & right -1;
+      
+      return right &left;
+      								// 12 ---- 1100
+									// 11 ---- 1011
+									// 10 ---- 1010
+									// 9  ---- 1001
+									// 8  ---- 1000
+									// 7  ---- 0111
+									// 6  ---- 0110
+									// 5  ---- 0101
+									
+
     }
 }
