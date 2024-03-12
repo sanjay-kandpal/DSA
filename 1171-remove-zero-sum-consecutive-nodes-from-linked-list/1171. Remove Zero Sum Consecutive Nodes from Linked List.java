77276c70ea728ff4aa2228188561cdf9d7b7162e
Sanjay kandpal <72781686+sanjay-kandpal@users.noreply.github.com>
2024-03-12 21:00:22 +0530

    Runtime: 2 ms (89.13%), Memory: 43.9 MB (61.26%) - Gitcode

--- 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171. Remove Zero Sum Consecutive Nodes from Linked List.java
+++ 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171. Remove Zero Sum Consecutive Nodes from Linked List.java
@@ -11,20 +11,42 @@
 class Solution {
     public ListNode removeZeroSumSublists(ListNode head) {
         ListNode front = new ListNode(0,head);
-        ListNode start = front;
-        
-        while(start != null){
-            int prefixSum = 0;
-            ListNode end = start.next;
-            while(end != null){
-                prefixSum += end.val;
-                if(prefixSum == 0){
-                    start.next = end.next;
-                }
-                end = end.next;
-            }
-            start = start.next;
+        ListNode current = front;
+        int prefixSum = 0;
+        Map<Integer,ListNode> prefixSumToNode = new HashMap<>();
+        // Calculate the prefix sum for each node and add to the hashmap
+        // Duplicate prefix sum values will be replaced
+        while(current != null){
+            prefixSum += current.val;
+            prefixSumToNode.put(prefixSum,current);
+            current = current.next;
+        }
+        // Reset prefix sum and current
+        prefixSum = 0;
+        current = front;
+        // Delete zero sum consecutive sequences 
+        // by setting node before sequence to node after
+        while(current != null){
+            prefixSum += current.val;
+            current.next = prefixSumToNode.get(prefixSum).next ;
+            current = current.next;
         }
         return front.next;
+        // ListNode front = new ListNode(0,head);
+        // ListNode start = front;
+        
+        // while(start != null){
+        //     int prefixSum = 0;
+        //     ListNode end = start.next;
+        //     while(end != null){
+        //         prefixSum += end.val;
+        //         if(prefixSum == 0){
+        //             start.next = end.next;
+        //         }
+        //         end = end.next;
+        //     }
+        //     start = start.next;
+        // }
+        // return front.next;
     }
 }
