2949954de9545185eebaef1c6c45eb3ae8b32e07
Sanjay kandpal <72781686+sanjay-kandpal@users.noreply.github.com>
2024-03-21 17:31:11 +0530

    Runtime: 0 ms (100.00%), Memory: 44.8 MB (9.38%) - Gitcode

---
+++ 328-odd-even-linked-list/328. Odd Even Linked List.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode first = head;
+        ListNode second = head.next;
+        ListNode evenHead = second;
+
+        while(second != null && second.next != null){
+            first.next = second.next;
+            first = second.next;
+            second.next = first.next;
+            second = second.next;
+        }
+        first.next = evenHead;
+        return head;
+    }
+}
