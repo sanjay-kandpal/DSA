a9d823ec2216b4bc891838219708111b7578e191
Sanjay kandpal <72781686+sanjay-kandpal@users.noreply.github.com>
2024-03-27 23:19:59 +0530

    Runtime: 4 ms (100.00%), Memory: 48.2 MB (9.20%) - Gitcode

--- 713-subarray-product-less-than-k/713. Subarray Product Less Than K.java
+++ 713-subarray-product-less-than-k/713. Subarray Product Less Than K.java
@@ -1,25 +1,42 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-        int p1 = 0,p2 =0;
-        int count =0;
-        while(p1 < nums.length ){
-            int MultiSum = 1;
-            p2 = p1;
-            while(p2 < nums.length){
-                if(p1 == p2){
-                    MultiSum = nums[p1];
-                }else{
-                    MultiSum *= nums[p2]; 
-                }
-                if(MultiSum < k){
-                    count++;
-                    p2++;
-                }else{
-                    break;
-                }
-            }
-            p1++;
+        // BruteForce Solution:
+        // int p1 = 0,p2 =0;
+        // int count =0;
+        // while(p1 < nums.length ){
+        //     int MultiSum = 1;
+        //     p2 = p1;
+        //     while(p2 < nums.length){
+        //         if(p1 == p2){
+        //             MultiSum = nums[p1];
+        //         }else{
+        //             MultiSum *= nums[p2]; 
+        //         }
+        //         if(MultiSum < k){
+        //             count++;
+        //             p2++;
+        //         }else{
+        //             break;
+        //         }
+        //     }
+        //     p1++;
+        // }
+        // return count;
+        // Optimised Solution:
+        if( k < 1) return 0;
+        int start = 0,end = 0,product = 1,subcount = 0;
+        while(start < nums.length && end < nums.length){
+            product *= nums[end];
+            
+                while(product >= k && start <= end){
+                    product /= (nums[start]);
+                    start++;
+                } 
+            
+            subcount += end -start +1;
+                end++;
         }
-        return count;
+        return subcount;
+
     }
 }
