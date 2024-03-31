4cd3cdb6bc0d0cc644e3cb54a362e6791339cd0f
Sanjay kandpal <72781686+sanjay-kandpal@users.noreply.github.com>
2024-03-31 10:18:44 +0530

    Runtime: 6 ms (96.65%), Memory: 48 MB (24.60%) - Gitcode

--- 49-group-anagrams/49. Group Anagrams.java
+++ 49-group-anagrams/49. Group Anagrams.java
@@ -1,17 +1,39 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String,List<String>> map = new HashMap<>();
+    //    List<List<String>> ls = new ArrayList<>();
+    //    if(strs.length == 1){
+    //        List<String> ans = new ArrayList<>();
+    //        ans.add(strs[0]);
+    //        ls.add(ans);
+    //        return ls;
+    //    } 
 
-        for(String word : strs){
-            char[] chars = word.toCharArray();
-            Arrays.sort(chars);
-            String sortedWord = new String(chars);
+    //    Map<String,List<String>> mp = new HashMap<>();
+       
+    //    for(int i = 0 ; i < strs.length ; i++){
+    //        String s = strs[i];
+    //        char[] charArray = s.toCharArray();
+           
+    //        Arrays.sort(charArray);
 
-            if(!map.containsKey(sortedWord)){
-                map.put(sortedWord,new ArrayList<>());
-            }
-            map.get(sortedWord).add(word);
+    //        StringBuilder newString = new StringBuilder();
+    //        newString.append(charArray);
+    //        mp.computeIfAbsent(newString.toString(), k -> new ArrayList<>()).add(s);
+    //    }
+    //    return new ArrayList<>(mp.values());
+
+    // Try By Yourself
+    Map<String,List<String>> map = new HashMap<>();
+    for(String words : strs){
+        char[] word = words.toCharArray();
+        Arrays.sort(word);
+        String sortedWord = new String(word);
+        if(!map.containsKey(sortedWord)){
+           map.put(sortedWord,new ArrayList<>()); 
         }
-        return new ArrayList<>(map.values());
+        map.get(sortedWord).add(words);
+    }
+    return new ArrayList<>(map.values());
+    
     }
 }
