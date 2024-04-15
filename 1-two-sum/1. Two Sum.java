88ed594cfd42a4740619b2655e2e55e0d0bec523
Sanjay kandpal <72781686+sanjay-kandpal@users.noreply.github.com>
2024-04-15 18:35:21 +0530

    Runtime: 2 ms (97.63%), Memory: 44.4 MB (94.44%) - Gitcode

--- 1-two-sum/1. Two Sum.java
+++ 1-two-sum/1. Two Sum.java
@@ -1,18 +1,40 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int i = 0 , j= 0 ;
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int count = 0;
+        
+    // Try Yourself
+    int x = 0; 
+    Map<Integer,Integer> map = new HashMap<>();
+    for(int i = 0 ; i < nums.length ; i++){
+        if(map.containsKey(target-nums[i])){
+          return new int[]{map.get(target-nums[i]),i};
+        }
+        map.put(nums[i],i);
+    }
+    return null;
+
+
+
+
+
+
+
+
+
+        
+        
+        // int i = 0 , j= 0 ;
+        // HashMap<Integer,Integer> map = new HashMap<>();
+        // int count = 0;
         
        
-        for(j =0 ; j < nums.length; j++){
-            int element =target - nums[j];
+        // for(j =0 ; j < nums.length; j++){
+        //     int element =target - nums[j];
             
-            if(map.containsKey(element)){
-                return new int[]{map.get(element),j};
-            }
-            map.put(nums[j],j);
-        }
-        return null;
+        //     if(map.containsKey(element)){
+        //         return new int[]{map.get(element),j};
+        //     }
+        //     map.put(nums[j],j);
+        // }
+        // return null;
     }
 }
