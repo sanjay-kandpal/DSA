b18a1082a4a990dada32eb177469403c4001401a
Sanjay kandpal <72781686+sanjay-kandpal@users.noreply.github.com>
2024-05-04 23:21:23 +0530

    Runtime: 11 ms (36.49%), Memory: 47.3 MB (99.93%) - Gitcode

--- 169-majority-element/169. Majority Element.java
+++ 169-majority-element/169. Majority Element.java
@@ -1,16 +1,25 @@
 class Solution {
-    public int majorityElement(int[] nums) {
-        for(int i = 0 ; i < nums.length ; i++){
-            int k = nums[i];
-            int j = i -1;
-
-            while(j >= 0 && nums[j] < k ){
-                nums[j+1] = nums[j];
-                j--;
-            }
-            nums[j+1] = k;
+    private Map<Integer,Integer> countNums(int[] nums){
+        Map<Integer,Integer> counts = new HashMap<Integer,Integer>();
+        for(int num : nums){
+         if(counts.containsKey(num)){
+            counts.put(num,counts.get(num)+1);
+         }else{
+            counts.put(num,1);
+         }
         }
-        int mid = (nums.length)/2;
-        return nums[mid];
+        return counts;
+    }
+    public int majorityElement(int[] nums) {
+        Map<Integer,Integer> counts = countNums(nums);
+
+        Map.Entry<Integer,Integer> majorityEntry = null;
+               
+        for(Map.Entry<Integer,Integer> entry : counts.entrySet()){
+            if(majorityEntry == null || entry.getValue() > majorityEntry.getValue()){
+               majorityEntry = entry;
+            }
+        }
+        return majorityEntry.getKey();
     }
 }
