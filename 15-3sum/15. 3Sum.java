9b451a57a58659c9cd4ec58cb85c75212fbb8d89
Sanjay kandpal <72781686+sanjay-kandpal@users.noreply.github.com>
2024-07-14 18:32:44 +0530

    Runtime: 27 ms (86.04%), Memory: 52.7 MB (16.44%) - Gitcode

--- 15-3sum/15. 3Sum.java
+++ 15-3sum/15. 3Sum.java
@@ -1,30 +1,38 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> results = new ArrayList<>();
         Arrays.sort(nums);
-        int target = 0;
-        
-        List<List<Integer>> output = new ArrayList<>();
-        Set<List<Integer>> s = new HashSet<>();
-        for(int i = 0 ; i < nums.length ; i++){
-            int j = i + 1;
-            int k = nums.length - 1;
-            while(j < k){
-                
-                int sum = nums[i] + nums[j] + nums[k];
-                if(sum == target){
-                    s.add(Arrays.asList(nums[i],nums[j],nums[k]));
-                    j++;
-                    k--;
-                }else if(sum < target){
-                    j++;
-                }else{
-                    k--;
-                }
-            
+        int n = nums.length - 1;
+        for(int i = 0 ; i < n-1 ; i++){
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue; // Skip duplicate elements
             }
-
-        }
-        output.addAll(s);
-        return output;
+            int first = i;
+            int second = i+ 1;
+            int third = n;
+            while(second < third){
+                int sum = nums[first] + nums[second] + nums[third];
+                if(sum < 0){
+                    second++;
+                }else if(sum > 0){
+                    third--;
+                }else{
+                    List<Integer> triplet = new ArrayList<>();
+                    triplet.add(nums[first]);
+                    triplet.add(nums[second]);
+                    triplet.add(nums[third]);
+                    results.add(triplet);
+                    while(second < third && nums[second+1] == nums[second]){
+                        second++; //ignore duplicates
+                    }
+                    while(second < third && nums[third] == nums[third-1]){
+                        third--; //ignore duplicates
+                    }
+                    second++;
+                    third--;
+                }
+            }
+        }    
+        return results;
     }
 }
