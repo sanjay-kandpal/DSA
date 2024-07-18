4f42e595e31e22689535c84a0c070ea83110b86e
Sanjay kandpal <72781686+sanjay-kandpal@users.noreply.github.com>
2024-07-18 18:20:15 +0530

    Runtime: 11 ms (6.63%), Memory: 57.1 MB (37.09%) - Gitcode

--- 53-maximum-subarray/53. Maximum Subarray.java
+++ 53-maximum-subarray/53. Maximum Subarray.java
@@ -1,5 +1,7 @@
 class Solution {
     public int maxSubArray(int[] nums) {
+    // Divide and conqueror method
+
     // Kedane's Algorithm
     //    int max = Integer.MIN_VALUE;
     //    int sum =0;
@@ -20,27 +22,53 @@ class Solution {
         if(i == n){
             return nums[i];
         }
-
-        int mid = (i+n)/2;
-        int sum = 0,leftMaxSum = Integer.MIN_VALUE;
-
-        for(int l = mid ; l >= i; l--){
-            sum += nums[l];
-            if(sum > leftMaxSum ){
-                leftMaxSum = sum;
-            }
+        int sum = 0 ,leftSum =Integer.MIN_VALUE,rightSum = Integer.MIN_VALUE;
+        int mid = (i +n)/2;
+        for(int j = mid ;  j >= i ; j--){
+            sum += nums[j];
+            
+            // if(sum > leftSum ){
+            //     leftSum = sum;
+            // }
+            leftSum = Math.max(leftSum,sum);
         }
-
-        int rightMaxSum = Integer.MIN_VALUE;
         sum = 0;
-        for(int l = mid + 1 ; l <= n ;l++){
-            sum += nums[l];
-            if(sum > rightMaxSum){
-                rightMaxSum = sum;
-            }
+        for(int k = mid+1 ; k <=n ;k++){
+            sum += nums[k];
+            // if(sum > rightSum){
+            //     rightSum = sum;
+            // }
+            rightSum = Math.max(rightSum,sum);
         }
-        int maxLeftRight = Math.max(helper(nums,i,mid),helper(nums,mid+1,n));
-        return Math.max(maxLeftRight,rightMaxSum + leftMaxSum);
 
+        int maxSum = Math.max(helper(nums,i,mid),helper(nums,mid+1,n));
+        return Math.max(maxSum,leftSum+rightSum);
     }
+    // public static int helper(int[] nums,int i,int n){
+    //     if(i == n){
+    //         return nums[i];
+    //     }
+
+    //     int mid = (i+n)/2;
+    //     int sum = 0,leftMaxSum = Integer.MIN_VALUE;
+
+    //     for(int l = mid ; l >= i; l--){
+    //         sum += nums[l];
+    //         if(sum > leftMaxSum ){
+    //             leftMaxSum = sum;
+    //         }
+    //     }
+
+    //     int rightMaxSum = Integer.MIN_VALUE;
+    //     sum = 0;
+    //     for(int l = mid + 1 ; l <= n ;l++){
+    //         sum += nums[l];
+    //         if(sum > rightMaxSum){
+    //             rightMaxSum = sum;
+    //         }
+    //     }
+    //     int maxLeftRight = Math.max(helper(nums,i,mid),helper(nums,mid+1,n));
+    //     return Math.max(maxLeftRight,rightMaxSum + leftMaxSum);
+
+    // }
 }
