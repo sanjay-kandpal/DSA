565183df42de3bec7f1f9bb019a261019a9e6262
Sanjay kandpal <72781686+sanjay-kandpal@users.noreply.github.com>
2024-07-24 00:06:32 +0530

    Runtime: 11 ms (87.54%), Memory: 45.4 MB (8.93%) - Gitcode

---
+++ 875-koko-eating-bananas/875. Koko Eating Bananas.java
@@ -0,0 +1,24 @@
+//The following codes pass all cases
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1, right = 1000000000 + 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (f(piles, mid) <= h) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+    
+    private int f(int[] piles, int speed) {
+        int hours = 0;
+        for (int i = 0; i < piles.length; i++) {
+            hours += piles[i] / speed;
+            if (piles[i] % speed > 0) hours++;
+        }
+        return hours;
+    }
+}
