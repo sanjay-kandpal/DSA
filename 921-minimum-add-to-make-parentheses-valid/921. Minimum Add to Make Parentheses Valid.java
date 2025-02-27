c750904dc0728980f91b48e6dd4c1420e0cab76b
Sanjay kandpal <72781686+sanjay-kandpal@users.noreply.github.com>
2025-02-27 16:08:16 +0530

    Runtime: 2 ms (24.83%), Memory: 41.1 MB (98.09%) - Gitcode

--- 921-minimum-add-to-make-parentheses-valid/921. Minimum Add to Make Parentheses Valid.java
+++ 921-minimum-add-to-make-parentheses-valid/921. Minimum Add to Make Parentheses Valid.java
@@ -1,12 +1,15 @@
 class Solution {
     public int minAddToMakeValid(String s) {
         Stack<Character> st = new Stack<>();
-        for(int i = 0 ; i < s.length() ; i++){
-            if((!st.isEmpty() && st.peek() == '(') && s.charAt(i) == ')'){
+        int i = 0 ;
+        
+        while( i < s.length()){
+            if(!st.isEmpty() && st.peek() == '(' && s.charAt(i) == ')'){
                 st.pop();
             }else{
-                st.push(s.charAt(i));
+            st.push(s.charAt(i));
             }
+            i++;
         }
         return st.size();
     }
