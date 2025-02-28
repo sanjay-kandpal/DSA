697a98ccb92f52aed45f1a9c4e8b3990eab89d92
Sanjay kandpal <72781686+sanjay-kandpal@users.noreply.github.com>
2025-02-28 10:08:52 +0530

    Runtime: 496 ms (9.03%), Memory: 45.7 MB (42.29%) - Gitcode

--- 214-shortest-palindrome/214. Shortest Palindrome.java
+++ 214-shortest-palindrome/214. Shortest Palindrome.java
@@ -1,13 +1,13 @@
 class Solution {
     public String shortestPalindrome(String s) {
         int length = s.length();
-        String reversedString = new StringBuilder(s).reverse().toString();
+        String reverseString = new StringBuilder(s).reverse().toString();
 
-        for(int i = 0; i < length ; i++){
+        for(int i = 0; i< length ; i++){
             if(
-                s.substring(0,length-i).equals(reversedString.substring(i))
+                s.substring(0,length-i).equals(reverseString.substring(i))
             ){
-                return new StringBuilder(reversedString.substring(0,i))
+                return new StringBuilder(reverseString.substring(0,i))
                 .append(s)
                 .toString();
             }
